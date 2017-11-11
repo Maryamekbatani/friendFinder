0d937334a09d65b0c94531e78c074aa5f7fd787f
Maryam EKBATANI <maryamekbatani@Maryams-MacBook-Pro.local>
2017-11-11 09:31:12 -0500

    new files

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -26,13 +26,10 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
@@ -45,17 +42,51 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
     
       res.json(friends);
+
+      var userInput = req.body;
+      console.log('userInput =' + JSON.stringify(userInput));
+      var userResponses = userInput.scores;
+      console.log('userResponses =' + userResponses;
+
+      //Decide best friend match 
+
+      var canidate = '';
+      var canidateImage = '';
+      var totalDifference = 10000;
+
   });
+};
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+////////////////////////////////////////////////////
 
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
 
-    console.log(tableData);
+    // Examine all existing friends in the list
+    for (var i = 0; i < friends.length; i++) {
+      // console.log('friend = ' + JSON.stringify(friends[i]));
+
+      // Compute differenes for each question
+      var diff = 0;
+      for (var j = 0; j < userResponses.length; j++) {
+        diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+      }
+      // console.log('diff = ' + diff);
+
+      // If lowest difference, record the friend match
+      if (diff < totalDifference) {
+        // console.log('Closest match found = ' + diff);
+        // console.log('Friend name = ' + friends[i].name);
+        // console.log('Friend image = ' + friends[i].photo);
+
+        totalDifference = diff;
+        canidate = friends[i].name;
+        canidateImage = friends[i].photo;
+      }
+    }
+
+    // Add new user
+    friends.push(userInput);
+
+    // Send appropriate response
+    res.json({status: 'OK', canidateName: canidateName, canidateImage: canidateImage});
   });
 };
